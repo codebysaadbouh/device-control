aa9c7e6baa58d2a70c8f9a54f9222559a31e6510
codebysaadbouh <74369527+codebysaadbouh@users.noreply.github.com>
2021-02-22 08:48:51 +0100

    Gestion du Routing : simulation de l'authentification, gestion des routing, error 404 ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppareilsComponent } from './appareils/appareils.component';
-import { FormsModule } from '@angular/forms';
-
 import { AppareilService} from './services/appareil.service';
 import { AuthComponent } from './auth/auth.component';
 import { AppareilViewComponent } from './appareil-view/appareil-view.component';
+import {AuthService} from './services/auth.service';
+import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
+import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
+import {AuthGuard} from './services/auth-guard.service';
+
+const appRoutes: Routes = [
+  { path: 'appareils', canActivate: [AuthGuard], component: AppareilViewComponent },
+  { path: 'appareils/:id', component: SingleAppareilComponent},
+  { path: 'auth', component: AuthComponent },
+  { path: '', component: AppareilViewComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo : '/not-found'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     AppareilsComponent,
     AuthComponent,
-    AppareilViewComponent
+    AppareilViewComponent,
+    SingleAppareilComponent,
+    FourOhFourComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    AppareilService
+    AppareilService,
+    AuthService,
+    AuthGuard
   ],
   bootstrap: [AppComponent]
 })
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,32 +1,48 @@
 export class AppareilService {
+
   appareils = [
     {
-      name: 'machine à laver',
+      id: 0,
+      name: 'Smart TV - samsung',
       status : 'éteint'
     },
     {
-      name: 'Lave-vaisselle',
+      id: 1,
+      name: 'Lampe - salle à manger',
       status : 'allumé'
     },
     {
-      name: 'micro-onde',
+      id: 2,
+      name: 'Climatiseur - salon',
       status : 'éteint'
     }
-  ]
+  ];
+
+  //cette fonction va retourner l'appareil correspondant à un id renseigné en paramètre
+  getAppareilById(id: number){
+    return this.appareils.find(
+      (appareilObject) => {
+        return appareilObject.id === id;
+      }
+    );
+  }
 
   switchOnAll(){
      for(let appareil of this.appareils) {
        appareil.status = 'allumé';
      }
   }
+
   switchOffAll(){
     for(let appareil of this.appareils) {
       appareil.status = 'éteint';
     }
   }
+
   switchOnOne(index: number){
     this.appareils[index].status = 'allumé';
   }
+
   switchOffOne(index: number){
     this.appareils[index].status = 'éteint';
   }
